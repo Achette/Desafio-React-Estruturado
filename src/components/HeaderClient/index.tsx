5573f645fc503154c8f59680794cf125a01f752d
Achette <i.achette@live.com>
2024-01-16 15:46:58 -0300

    feat: cria componente HeaderClient e LoggedUser

---
+++ src/components/HeaderClient/index.tsx
@@ -0,0 +1,39 @@
+import React from 'react'
+import CartIcon from '../CartIcon'
+import LoggedUser from '../LoggedUser'
+import { Link } from 'react-router-dom'
+import iconAdmin from '../../assets/settings.svg'
+import { ContextToken } from '@/utils/context-token'
+import * as authService from '@/services/auth-service'
+import './styles.css'
+
+export default function HeaderClient() {
+  const { contextTokenPayload } = React.useContext(ContextToken)
+
+  return (
+    <header className="dsc-header-client">
+      <nav className="dsc-container">
+        <Link to={'/'}>
+          <h1>DSCommerce</h1>
+        </Link>
+        <div className="dsc-navbar-right">
+          <div className="dsc-menu-items-container">
+            {contextTokenPayload && authService.hasAnyRoles(['ROLE_ADMIN']) && (
+              <Link to="/admin">
+                <div className="dsc-menu-item">
+                  <img src={iconAdmin} alt="Admin" />
+                </div>
+              </Link>
+            )}
+            <Link to="/cart">
+              <div className="dsc-menu-item">
+                <CartIcon />
+              </div>
+            </Link>
+          </div>
+          <LoggedUser />
+        </div>
+      </nav>
+    </header>
+  )
+}
---
+++ src/components/LoggedUser/index.tsx
@@ -0,0 +1,26 @@
+import './styles.css'
+import React from 'react'
+import { Link } from 'react-router-dom'
+import { ContextToken } from '@/utils/context-token'
+import * as authService from '@/services/auth-service'
+
+export default function LoggedUser() {
+  const { contextTokenPayload, setContextTokenPayload } =
+    React.useContext(ContextToken)
+
+  function handleLogoutClick() {
+    authService.logout()
+    setContextTokenPayload(undefined)
+  }
+
+  return contextTokenPayload && authService.isAuthenticated() ? (
+    <div className="dsc-logged-user">
+      <p>{contextTokenPayload.user_name}</p>
+      <span onClick={handleLogoutClick}>Sair</span>
+    </div>
+  ) : (
+    <Link className="dsc-enter" to={'/login'}>
+      Entrar
+    </Link>
+  )
+}
